beb798e07146ac3bfa03ff8208164000a039ffe4
Sanchit Lohia <sanchitlohia27@gmail.com>
2017-08-27 02:42:30 +0530

    Adding test index file.

--- test.js
+++
@@ -1,8 +0,0 @@
-var ferror =  require('./index');
-var error_map = require('./sample_error_map');
-
-ferror.set_error_map(error_map);
-
-
-s = new ferror("DB_101");
-sconsole.log(s)
--- test/index.js
+++ test/index.js
@@ -1,33 +1,33 @@
-/* global describe, it, beforeEach, before, afterEach, after, done */
-"use strict";
-
-var 
-    should    = require('should'),
-    assert    = require('assert'),
-    request   = require('supertest'),
-
-    ferror    = require('../index'),
-    error_map = require('../sample_error_map');
-
-describe('Error framework Test', function(){
-    it('should get correct code and message', function(done){
-        ferror.set_error_map(error_map);
-
-        var error = new ferror("ERROR_101");
-        error.errorCode.should.equal("ERROR_101")
-        error.message.should.equal("Error has occured")
-        error.http_code.should.equal(400)
-        error.user_message.should.equal("Please try again later");
-        done();
-    });
-
-    it('should get correct code and message with message replaced correctly', function(done){
-        ferror.set_error_map(error_map);
-  
-        var error = new ferror("DB_101", {err : "Connection Error" , err2 : "DB error"});
-        error.msg.should.equal("Error in DB operation. Error is Connection Error")
-        error.master_code.should.equal(400);
-        error.user_message.should.equal("DB error")
-        done();
-    });
-});
+/* global describe, it, beforeEach, before, afterEach, after, done */ 
+"use strict"; 
+ 
+var  
+    should    = require('should'), 
+    assert    = require('assert'), 
+    request   = require('supertest'), 
+ 
+    ferror    = require('../index'), 
+    error_map = require('../sample_error_map'); 
+ 
+describe('Error framework Test', function(){ 
+    it('should get correct code and message', function(done){ 
+        ferror.set_error_map(error_map); 
+ 
+        var error = new ferror("ERROR_101"); 
+        error.errorCode.should.equal("ERROR_101") 
+        error.message.should.equal("Error has occured") 
+        error.http_code.should.equal(400) 
+        error.user_message.should.equal("Please try again later"); 
+        done(); 
+    }); 
+ 
+    it('should get correct code and message with message replaced correctly', function(done){ 
+        ferror.set_error_map(error_map); 
+   
+        var error = new ferror("DB_101", {err : "Connection Error" , err2 : "DB error"}); 
+        error.msg.should.equal("Error in DB operation. Error is Connection Error") 
+        error.master_code.should.equal(400); 
+        error.user_message.should.equal("DB error") 
+        done(); 
+    }); 
+}); 
